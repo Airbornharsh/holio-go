497a9eef1f4d830d788cf6b556f195c1b8aa0e18
Harsh Keshri <harshkeshri1234567@gmail.com>
2023-10-21 02:29:07 +0530

    Added Hotel Controllers

---
+++ controllers/HotelController.go
@@ -0,0 +1,117 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func CreateHotelHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "CreateHotelHandler",
+	})
+}
+
+func SearchHotelsHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "SearchHotelsHandler",
+	})
+}
+
+func GetPopularHotelsHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "GetPopularHotelsHandler",
+	})
+}
+
+func GetHotelHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "GetHotelHandler",
+	})
+}
+
+func UpdateHotelHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "UpdateHotelHandler",
+	})
+}
+
+func DeleteHotelHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "DeleteHotelHandler",
+	})
+}
+
+func ChangeHotelNameHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelNameHandler",
+	})
+}
+
+func ChangeHotelDescriptionHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelDescriptionHandler",
+	})
+}
+
+func ChangeHotelAddressHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelAddressHandler",
+	})
+}
+
+func ChangeHotelPhoneHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelPhoneHandler",
+	})
+}
+
+func ChangeHotelWebsiteHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelWebsiteHandler",
+	})
+}
+
+func ChangeHotelEmailHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelEmailHandler",
+	})
+}
+
+func ChangeHotelStarRatingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelStarRatingHandler",
+	})
+}
+
+func ChangeHotelAvgRatingHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelAvgRatingHandler",
+	})
+}
+
+func ChangeHotelAvgPriceHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelAvgPriceHandler",
+	})
+}
+
+func ChangeHotelLocationHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelLocationHandler",
+	})
+}
+
+func ChangeHotelFacilitiesHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelFacilitiesHandler",
+	})
+}
+
+func ChangeHotelRoomsHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelRoomsHandler",
+	})
+}
+
+func ChangeHotelAmenitiesHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "ChangeHotelAmenitiesHandler",
+	})
+}
